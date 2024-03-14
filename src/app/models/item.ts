6b123e4375f15cc6bf8163abba32e74e12227156
Mikolaj <mkmbaran@gmail.com>
2024-03-14 11:51:38 +0100

    Filter type extension done

--- src/app/models/item.ts
+++ src/app/models/item.ts
@@ -2,5 +2,6 @@ export interface Item {
   id: string;
   name: string;
   price: number;
+  type: string;
   quantity: number;
 }
--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -11,8 +11,28 @@ export class StoreComponent {
   constructor(private readonly appService: AppService){}
 
   items: Item[] = [];
+  onlyVeggiesNotFruit: boolean = false;
 
   async ngOnInit() {
     this.items = await this.appService.getItems();
+    this.filterItems();
+  }
+
+  toggleVeggiesNotFruit = () => {
+    this.onlyVeggiesNotFruit = !this.onlyVeggiesNotFruit;
+    this.filterItems();
+  }
+
+  async filterItems() {
+    if (this.onlyVeggiesNotFruit){
+      this.items = (await this.appService.getItems()).filter(
+        (item: any) => item.type === 'vegetable'
+      )
+    }
+    if (!this.onlyVeggiesNotFruit){
+      this.items = (await this.appService.getItems()).filter(
+        (item: any) => item.type === 'fruit'
+      )
+    }
   }
 }
